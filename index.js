2858d2b7b26b57280002d644a7da85a87e34f6d1
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-10-04 08:23:53 +0500

    3-dars

---
+++ index.js
@@ -0,0 +1,206 @@
+//1-masala
+//let a = 5;
+//if (a > 0) {
+//    console.log("a soni musbat.");
+//} else {
+//    console.log("a soni musbat emas.");
+//}
+//2-masala
+//let a = 10; 
+//if (a % 2 == 0) {
+//    console.log("a soni juft son.");
+//} else {
+//    console.log("a soni juft emas.");
+//}
+//3-masla
+//let a = 7
+//if (a % 2 != 0) {
+//    console.log("a soni toq son.");
+//} else {
+//    console.log("a soni toq emas.");
+//}
+//6-masala
+//let a = 1
+//let b = 2
+//let c = 3
+//if (a < b && b < c) {
+//    console.log("a < b va b < c");
+//} else {
+//    console.log("Shart bajarilmadi.");
+//}
+//7-masala
+//let a = 8 
+//let b = 6 
+//let c = 5
+//if (a > 0 && b > 0 && c > 0) {
+ //   console.log("Barcha sonlar musbat.");
+//} else {
+//    console.log("Barcha sonlar musbat emas.");
+//}
+//8-masla
+//let a = 8 
+//let b = 6
+//if (a > 0 && b > 0) {
+//    console.log("a va b sonlari musbat.");
+//} else {
+//    console.log("a va b sonlari musbat emas.");
+//}
+//9-masala
+//let a = 5 
+//let b = 7
+//if (a % 2 != 0 || b % 2 != 0) {
+//    console.log("a yoki b sonlaridan biri toq.");
+//} else {
+//   console.log("Ikkalasi ham juft.");
+//}
+//10-masala
+//let a = 4
+//let b = 7
+//if ((a % 2 == 0 && b % 2 != 0) || (a % 2 != 0 && b % 2 == 0)) {
+//    console.log("a va b sonlaridan faqat bittasi toq.");
+//} else {
+//    console.log("Shart bajarilmadi.");
+//}
+//11-masla
+//let a = 5 
+//let b = 3
+//if (a > 0 && b > 0 || a <= 0 && b <= 0) {
+//    console.log("a va b sonlari ikkisi ham musbat yoki ikkisi ham manfiy.");
+//} else {
+//    console.log("Shart bajarilmadi.");
+//}
+//12-masala
+//let a = 3 
+//let b = 4 
+//let c = 5
+//if (a > 0 && b > 0 && c > 0 && (a % 2 == 0 || b % 2 == 0 || c % 2 == 0)) {
+//    console.log("a, b yoki c sonlaridan biri musbat va juft.");
+//} else {
+//    console.log("Shart bajarilmadi.");
+//}
+//13-masala
+//let a = 4 
+//let b = 6 
+//let c = 9
+//if (a % 2 == 0 && b % 2 == 0 && c % 2 != 0) {
+//    console.log("a va b juft, c toq.");
+//} else {
+//    console.log("Shart bajarilmadi.");
+//}
+//14 masala
+//let a = 3 
+//let b = 7 
+//let c = 9 
+//if (a % 2 != 0 && b % 2 != 0 && c % 2 != 0) {
+//    console.log("a, b, c sonlarining hammasi toq.");
+//} else {
+//    console.log("Shart bajarilmadi.");
+//}
+//15 masala
+//let a = 4
+//let b = 2
+//let c = 6
+//if (a > 0 && b > 0 && c > 0 && a % 2 == 0 && b % 2 == 0) {
+//    console.log("a va bmusbat va juft.");
+//} else {
+//    console.log("Shart bajarilmadi.");
+//}
+//1misol
+//let counter=0
+//let son= +prompt("sonni kiriting")
+//let raqam;
+//if(raqam % 2 == 0){
+  //  raqam = raqam + 1
+//}
+//2-misol
+//let counter = 0
+//let son= +prompt("sonni kiriting")
+//let raqam;
+//if(raqam % 2 == 0){
+  //  raqam = raqam+1
+//}
+//else{
+  //  raqam = raqam-2
+//}
+//3-misol
+//let counter = 0
+//let son1 = +prompt("1-sonni kiriting")
+//let son1 = +prompt("1-sonni kiriting")
+//let max;
+//if(son1 > son2){
+  //  max = son1
+//}else{
+  //  max = son2
+//}
+//console.log(max)
+
+//4-misol
+//let counter=0
+//let son1 = +prompt("1-sonni kiriting")
+//let son2 = +prompt("2-sonni kiriting")
+//let max;
+//let min;
+//if(son1 > son2){
+//    max = son1
+//}
+//else{
+//    max = son2
+//}
+//if(son1 < son2){
+//    min = son1
+//}else{
+//    min = son1
+//}
+//console.log(max)
+//console.log(min)
+//5-misol
+//let counter=0
+//let num1 = +prompt("1-sonni kiri ting")
+//let num2 = +prompt("2-sonni kiri ting")
+//let num3 = +prompt("3-sonni kiri ting")
+//let max;
+//if(num1 = num2 + num3){
+//max = num1
+//}
+//else{
+//  max = num2
+//}else{
+//  max = num3
+//}6-misol
+//let counter =0
+//let son1 = +prompt("1-sonni kiriting")
+//let son2 = +prompt("2-sonni kiriting")
+//let son3 = +prompt("3-sonni kiriting")
+//let max;
+//let min;
+//if(son1 > son2){
+//    max = son1
+//}
+//else{
+//    max = son2
+//}
+//if(son1 < son2){
+//    min = son1
+//}else{
+//    min = son1
+//}
+//if(son2 < son3){
+//  max = son3
+//}
+//else{
+//  min = son3
+//}
+//console.log(max)
+//console.log(min)
+//7-misol
+//let sum = 0
+//let son1 = +prompt("1-sonni kiriting")
+//let son2 = +prompt("2-sonni kiriting")
+//let son3 = + prompt("3-sonni kiriting")
+//let ortacha;
+//if (son1 + son2+son3){ 
+//  ortacha = son1 + son2 + son3
+//  sum= ortacha / 3
+//}
+//console.log(sum);
+//
